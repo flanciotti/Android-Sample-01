e35b7f445254710df9c5dfb908df10fa2e7f2ec1
Fabio Lanciotti <flanciotti.it@gmail.com>
2016-09-19 13:44:24 +0200

    Step 02
    Added:
    - Resources;
    - Buttons;
    
    Java:
    - Import;
    - Subclass;
    - Casting;

--- app/src/main/java/android/lanciotti/com/android_sample_01/MainActivity.java
+++ app/src/main/java/android/lanciotti/com/android_sample_01/MainActivity.java
@@ -2,12 +2,30 @@ package android.lanciotti.com.android_sample_01;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        Button button = null;
+
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        button = (Button) this.findViewById( R.id.button );
+        button.setText( "modificato-" + button.getText() );
+
+
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                ((Button) view).setText( "click!" );
+            }
+        });
+
+
+
     }
 }
